9d20ecae78edb90d69f21a64eec583048c4a36d6
Leon Kappes <leon@kappes.space>
2021-09-20 13:15:43 +0200

    Remove old Kubernetes configs

--- k8s/registry-secret.sh
+++
@@ -1,5 +0,0 @@
-kubectl -n gitlab-auth create secret docker-registry gitlab-auth \
---docker-server=https://registry.gitlab.com \
---docker-username=xxx \
---docker-password=xxx \
---docker-email=xxx
